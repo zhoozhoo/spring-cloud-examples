4eafcd62399c609ff461fa625cfd39788591f4d1
Zhubin Salehi <zhoozhoo@yahoo.com>
2021-05-25 09:31:48 -0400

    Updated spring profile properties

--- spring-cloud-api-gateway/src/main/java/ca/zhoozhoo/springcloud/api/config/SecurityConfig.java
+++ spring-cloud-api-gateway/src/main/java/ca/zhoozhoo/springcloud/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ca.zhoozhoo.springcloud.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
@@ -10,6 +11,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode;
 
 @Configuration
+@EnableReactiveMethodSecurity
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
--- spring-cloud-api-gateway/src/test/java/ca/zhoozhoo/springcloud/api/ApiGatewayApplicationTests.java
+++ spring-cloud-api-gateway/src/test/java/ca/zhoozhoo/springcloud/api/ApiGatewayApplicationTests.java
@@ -2,7 +2,9 @@ package ca.zhoozhoo.springcloud.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
